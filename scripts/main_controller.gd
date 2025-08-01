e0f6f89c1dfb06c892482fc0cc994a83acd0ccd1
eac-nathan-price <40072816+eac-nathan-price@users.noreply.github.com>
2025-07-31 23:28:12 -0700

    timeline

--- scripts/main_controller.gd
+++ scripts/main_controller.gd
@@ -11,7 +11,6 @@ var total_sixteenth_notes = 64  # 4 bars * 16 sixteenth notes
 var beat_time = 60.0 / bpm
 var sixteenth_note_time = beat_time / 4.0
 var current_time = 0.0
-var is_recording = false
 var is_playing = false
 var recording_start_time = 0.0
 var recording_elapsed_time = 0.0
@@ -23,6 +22,13 @@ var tracks = {
 	"jump": []
 }
 
+# Recording state for each track
+var recording_tracks = {
+	"move_right": false,
+	"move_left": false,
+	"jump": false
+}
+
 # Input mapping - we'll use any key press for recording
 var input_map = {
 	"move_right": "ui_right",
@@ -31,11 +37,15 @@ var input_map = {
 }
 
 # UI References
-@onready var record_button = $BottomHalf/DAWHeader/RecordButton
 @onready var play_button = $BottomHalf/DAWHeader/PlayButton
 @onready var stop_button = $BottomHalf/DAWHeader/StopButton
 @onready var bpm_display = $BottomHalf/DAWHeader/BPMDisplay
 @onready var click_track = $ClickTrack
+
+# Record buttons for each track
+@onready var record_right_button = $BottomHalf/TimelineContainer/TimelineGrid/TimelineTracks/MoveRightTrack/RecordRightButton
+@onready var record_left_button = $BottomHalf/TimelineContainer/TimelineGrid/TimelineTracks/MoveLeftTrack/RecordLeftButton
+@onready var record_jump_button = $BottomHalf/TimelineContainer/TimelineGrid/TimelineTracks/JumpTrack/RecordJumpButton
 
 # Character reference
 @onready var character = $TopHalf/SubViewport/GameWorld/CharacterBody2D
@@ -51,7 +61,9 @@ func _ready():
 			tracks[track_name].append(false)
 	
 	# Setup UI
-	record_button.pressed.connect(_on_record_pressed)
+	record_right_button.pressed.connect(_on_record_right_pressed)
+	record_left_button.pressed.connect(_on_record_left_pressed)
+	record_jump_button.pressed.connect(_on_record_jump_pressed)
 	play_button.pressed.connect(_on_play_pressed)
 	stop_button.pressed.connect(_on_stop_pressed)
 	
@@ -78,7 +90,7 @@ func _create_timeline_ui():
 		
 		for i in range(total_sixteenth_notes):
 			var button = Button.new()
-			button.custom_minimum_size = Vector2(30, 30)
+			button.custom_minimum_size = Vector2(8, 25)  # Much narrower buttons
 			button.text = ""
 			button.toggle_mode = true
 			button.pressed.connect(_on_timeline_button_pressed.bind(track_name, i))
@@ -90,48 +102,89 @@ func _setup_click_track():
 	# The click track will be set up when needed during playback
 	pass
 
-func _on_record_pressed():
-	if not is_recording and not is_playing:
-		_start_recording()
-	elif is_recording:
-		_stop_recording()
+func _on_record_right_pressed():
+	if not is_playing:
+		if not recording_tracks["move_right"]:
+			_start_recording_track("move_right")
+		else:
+			_stop_recording_track("move_right")
+
+func _on_record_left_pressed():
+	if not is_playing:
+		if not recording_tracks["move_left"]:
+			_start_recording_track("move_left")
+		else:
+			_stop_recording_track("move_left")
+
+func _on_record_jump_pressed():
+	if not is_playing:
+		if not recording_tracks["jump"]:
+			_start_recording_track("jump")
+		else:
+			_stop_recording_track("jump")
 
 func _on_play_pressed():
-	if not is_recording and not is_playing:
+	# Check if any track is recording
+	var any_recording = false
+	for track_name in recording_tracks.keys():
+		if recording_tracks[track_name]:
+			any_recording = true
+			break
+	
+	if not any_recording and not is_playing:
 		_start_playback()
 	elif is_playing:
 		_stop_playback()
 
 func _on_stop_pressed():
-	if is_recording:
-		_stop_recording()
+	# Stop all recording
+	for track_name in recording_tracks.keys():
+		if recording_tracks[track_name]:
+			_stop_recording_track(track_name)
 	if is_playing:
 		_stop_playback()
 
-func _start_recording():
-	is_recording = true
+func _start_recording_track(track_name):
+	recording_tracks[track_name] = true
 	recording_start_time = Time.get_ticks_msec() / 1000.0
 	recording_elapsed_time = 0.0
-	record_button.text = "Stop Recording"
-	play_button.disabled = true
 	
-	# Clear previous recording
-	for track_name in tracks.keys():
-		for i in range(total_sixteenth_notes):
-			tracks[track_name][i] = false
-			timeline_buttons[track_name][i].button_pressed = false
-			timeline_buttons[track_name][i].modulate = Color.WHITE
+	# Update button text
+	match track_name:
+		"move_right":
+			record_right_button.text = "Stop"
+		"move_left":
+			record_left_button.text = "Stop"
+		"jump":
+			record_jump_button.text = "Stop"
+	
+	# Clear previous recording for this track
+	for i in range(total_sixteenth_notes):
+		tracks[track_name][i] = false
+		timeline_buttons[track_name][i].button_pressed = false
+		timeline_buttons[track_name][i].modulate = Color.WHITE
 
-func _stop_recording():
-	is_recording = false
-	record_button.text = "Record"
-	play_button.disabled = false
+func _stop_recording_track(track_name):
+	recording_tracks[track_name] = false
+	
+	# Update button text
+	match track_name:
+		"move_right":
+			record_right_button.text = "Record"
+		"move_left":
+			record_left_button.text = "Record"
+		"jump":
+			record_jump_button.text = "Record"
 
 func _start_playback():
 	is_playing = true
 	current_time = 0.0
 	play_button.text = "Stop Playback"
-	record_button.disabled = true
+	
+	# Disable all record buttons
+	record_right_button.disabled = true
+	record_left_button.disabled = true
+	record_jump_button.disabled = true
 	
 	# Reset character position
 	character.position = Vector2(576, 200)
@@ -140,14 +193,32 @@ func _stop_playback():
 	is_playing = false
 	current_time = 0.0
 	play_button.text = "Play"
-	record_button.disabled = false
+	
+	# Enable all record buttons
+	record_right_button.disabled = false
+	record_left_button.disabled = false
+	record_jump_button.disabled = false
 
 func _on_timeline_button_pressed(track_name, index):
-	if not is_recording:
+	# Check if any track is recording
+	var any_recording = false
+	for track_name_check in recording_tracks.keys():
+		if recording_tracks[track_name_check]:
+			any_recording = true
+			break
+	
+	if not any_recording:
 		tracks[track_name][index] = timeline_buttons[track_name][index].button_pressed
 
 func _process(delta):
-	if is_recording:
+	# Check if any track is recording
+	var any_recording = false
+	for track_name in recording_tracks.keys():
+		if recording_tracks[track_name]:
+			any_recording = true
+			break
+	
+	if any_recording:
 		_process_recording(delta)
 	elif is_playing:
 		_process_playback(delta)
@@ -158,10 +229,12 @@ func _process_recording(delta):
 	
 	# Check for ANY key press during current sixteenth note
 	if Input.is_anything_pressed():
-		# Record on the move_right track for any key press
-		tracks["move_right"][current_sixteenth] = true
-		timeline_buttons["move_right"][current_sixteenth].button_pressed = true
-		timeline_buttons["move_right"][current_sixteenth].modulate = Color.GREEN
+		# Record on all currently recording tracks
+		for track_name in recording_tracks.keys():
+			if recording_tracks[track_name]:
+				tracks[track_name][current_sixteenth] = true
+				timeline_buttons[track_name][current_sixteenth].button_pressed = true
+				timeline_buttons[track_name][current_sixteenth].modulate = Color.GREEN
 
 func _process_playback(delta):
 	current_time += delta
